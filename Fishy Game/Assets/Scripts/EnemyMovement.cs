d6accb9391c7b6516ebb872c888eee04f0be9f5f
Braden <braden.gerace@hotmail.com>
2021-11-22 21:20:12 -0500

    enemy collider spawn delay
    added a 1 sec delay on collider when enemies spawn

--- Fishy Game/Assets/Scripts/EnemyMovement.cs
+++ Fishy Game/Assets/Scripts/EnemyMovement.cs
@@ -12,10 +12,13 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField]
     private Rigidbody2D enemyRb;
 
+    public Collider2D enemyCollider;
+
     // Start is called before the first frame update
     void Start()
     {
         enemyRb.velocity = RandomVector(minSpeed, maxSpeed);
+        StartCoroutine(ActiveRb(1));
     }
 
     private Vector3 RandomVector(float min, float max)
@@ -24,5 +27,12 @@ public class EnemyMovement : MonoBehaviour
         var y = Random.Range(min, max);
         var z = Random.Range(min, max);
         return new Vector3(x, y, z);
+    }
+
+    IEnumerator ActiveRb(float time)
+    {
+        enemyCollider.enabled = false;
+        yield return new WaitForSeconds(time);
+        enemyCollider.enabled = true;
     }
 }
--- Fishy Game/Assets/Scripts/SpawnManager.cs
+++ Fishy Game/Assets/Scripts/SpawnManager.cs
@@ -44,9 +44,9 @@ public class SpawnManager : MonoBehaviour
                 yield return new WaitForSeconds(spawnRate);
 
                 float spawnY = Random.Range
-                    (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, (Screen.height + 10))).y);
+                    (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, (Screen.height))).y);
                 float spawnX = Random.Range
-                    (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2((Screen.width + 10), 0)).x);
+                    (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2((Screen.width), 0)).x);
 
                 Vector2 spawnPosition = new Vector2(spawnX, spawnY);
                 
